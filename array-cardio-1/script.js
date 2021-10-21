7fdb14033780355ad64f46b7a21a17ebbb8a2820
nooranshah <nooranshahamri123@gmail.com>
2021-10-21 23:41:33 +0300

    array cardio day 2

--- array-cardio-1/script.js
+++ array-cardio-1/script.js
@@ -75,20 +75,20 @@ window.onload = () => {
 
   // Array.prototype.map()
   // 2. Give us an array of the inventor first and last names
-//   const fullNames = inventors.map(
-//     (inventor) => `${inventor.first} ${inventor.last}`
-//   );
-//   console.log(fullNames);
+  const fullNames = inventors.map(
+    (inventor) => `${inventor.first} ${inventor.last}`
+  );
+  console.log(fullNames);
 
   // Array.prototype.sort()
   // 3. Sort the inventors by birthdate, oldest to youngest
-  // const ordered = inventors.sort(function(a, b) {
-  //   if(a.year > b.year) {
-  //     return 1;
-  //   } else {
-  //     return -1;
-  //   }
-  // });
+  const ordered = inventors.sort(function(a, b) {
+    if(a.year > b.year) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
 
 //   const ordered = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
 //   console.table(ordered);
